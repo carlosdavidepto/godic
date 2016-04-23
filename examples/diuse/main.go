56ceecb36dedae9ac191df930171d2a26e80f00d
Carlos Davide <carlosdavidepto@13brane.net>
2016-04-23 12:46:32 -0400

    go fmt all the things!

--- examples/diuse/main.go
+++ examples/diuse/main.go
@@ -5,17 +5,17 @@ import "fmt"
 type A struct{}
 
 type B struct {
-  a *A
+	a *A
 }
 
 type C struct {
-  a *A
-  b *B
+	a *A
+	b *B
 }
 
 type D struct {
-  b *B
-  c *C
+	b *B
+	c *C
 }
 
 //go:generate sh -c "go run ../digen/main.go > dicontainer.go && go fmt dicontainer.go"
